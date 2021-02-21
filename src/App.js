7548456241a23210cf8530423a9d765431db2c15
Patrin <patrin.sinteppadon@gmail.com>
2021-02-20 21:15:44 -0800

    create simple main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
+import conch from './magicconch.jpg';
 import './App.css';
 
+const responses = [
+    "no",
+    "yes",
+    "maybe",
+    "try asking again",
+    "perhaps",
+    "it depends",
+    "game over",
+];
 function App() {
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // clear the 'question' textbox
+
+    // find a RANDOM value from the 'responses' array
+    let randomResponse = Math.floor(Math.random() * responses.length) + 1;
+    
+    console.log(randomResponse);
+
+    // set the text value of 'response' to be the result of step 2
+    document.getElementById('response').innerText = responses[randomResponse];
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <div>
+          <p id="response" style={{ fontSize: '75px', position: 'absolute', left: '500px'}}>
+            
+          </p>
+          <img src={conch} style={{ height: '500px'}} alt="the magic conch" />
+        </div>
+        
+        <p className="title-prompt">
+          Ask the Magic Conch
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{ marginBottom: '50px' }}>
+          <input id="question" style={{ marginRight: '10px' }}>
+          
+          </input>
+          <button onClick={handleSubmit}>
+            Ask
+          </button>
+        </div>
+        
       </header>
     </div>
   );
 }
-
 export default App;
